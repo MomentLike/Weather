73daebcc601bd0dd3df96a76a98250b7c99276b0
Moon <778155840@qq.com>
2019-04-23 14:00:27 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/app/weather/model/Province.java
@@ -0,0 +1,37 @@
+package com.app.weather.model;
+
+/**
+ * Created by S on 2019/4/23.
+ */
+public class Province {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+
+
+}
